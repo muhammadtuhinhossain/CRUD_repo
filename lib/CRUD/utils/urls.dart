4f2d826d39e5ffc169ef7cab3e89fc3e7863d8b2
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2026-02-28 14:05:44 +0600

    urls ui

---
+++ lib/CRUD/utils/urls.dart
@@ -0,0 +1,7 @@
+class Urls{
+  static String baseUrl='http://35.73.30.144:2008/api/v1';
+  static String readProduct='$baseUrl/ReadProduct';
+  static String deleteProduct(String id)=>'$baseUrl/DeleteProduct/$id';
+  static String createProduct='$baseUrl/CreateProduct';
+  static String updateProduct(String id)=> '$baseUrl/UpdateProduct/$id';
+}
